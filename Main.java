51b31a5252716942a82f26e3d8e3f78e1e8bbcbe
vonib <65134261+vonib@users.noreply.github.com>
2020-10-27 03:16:22 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,10 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// data structure is created here
-		User newUser = new User(,,);
-		// add new user to data structure 
-		//
-		// TODO Auto-generated method stub
+		User newUser = new User();
+		newUser.addNewUserProfile("bukky", "bukky@", "39747");
+		newUser.displayUserProfile(1);
 
 	}
 
--- User.java
+++ User.java
@@ -1,102 +1,79 @@
 import java.time.LocalDate;
+import java.util.HashMap;
 
 public class User {
-    private static class User {
+	//start the userIds from 1. 
+	private int userId = 1;
+
+    private static class UserDetails {
         private String userName;
         private String userEmail;
         private String phoneNumber;
         private LocalDate joiningDate;
 
-        public User(String userName, String userEmail, String phoneNumber, LocalDate joiningDate) {
+        public UserDetails(String userName, String userEmail, String phoneNumber, LocalDate joiningDate) {
             this.userName = userName;
             this.userEmail = userEmail;
             this.phoneNumber = phoneNumber;
             this.joiningDate = joiningDate;
         }
 
-        public changeName(String newName) {
-            this.userName = newName
-        }
-
-        public changeEmail(String newEmail) {
-            this.userEmail = newEmail
-        }
-
-        public changePhoneNumber(String newNumber) {
-            this.phoneNumber = newNumber
-        }
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getUserEmail() {
+			return userEmail;
+		}
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
+		public String getPhoneNumber() {
+			return phoneNumber;
+		}
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+		public LocalDate getJoiningDate() {
+			return joiningDate;
+		}
+		public void setJoiningDate(LocalDate joiningDate) {
+			this.joiningDate = joiningDate;
+		}
     }
 
-    // data structure to store user info - hashmap (key - userId, values - User)
-    private int userId = 1;
-    private HashMap<Integer, User> userMap = new HashMap<>();
+
+    private HashMap<Integer, UserDetails> userMap = new HashMap<Integer, UserDetails>();
 
     public void addNewUserProfile(String name, String emailAddress, String phoneNumber) {
-        //generate id - go through the integers (1 , 2, ..)
-        // create a new instance of user
-        //add to data structure storing users
+    	LocalDate today = LocalDate.now();
+    	UserDetails userDetails = new UserDetails(name, emailAddress, phoneNumber, today);
+    	userMap.put(userId, userDetails);
+    	userId++;
     }
 
-    //displayuserprofile -
-    // print formatted info
-    public void displayUserProfile(String userID) {
-
+    public void displayUserProfile(int userID) {
+    	UserDetails details = userMap.get(userID);
+    	System.out.println("User Id: " + userID + "," + " User Name: " + details.getUserName() + "," + " Email Address: " + 
+    	details.getUserEmail() + "," + " Phone Number: " + details.getPhoneNumber() + "," + " Joining Date: " + details.getJoiningDate());
     }
 
-    //change user name - get info hash map
-    public changeUserName(String userID, String newName) {
-        userMap.get(userID).changeName(newName)
+    public void changeUserName(int userID, String newName) {
+        userMap.get(userID).setUserName(newName);
     }
 
     //change user Email Address - get info hash map
-    public changeUserEmailAddress(String userID, String newEmailAddress) {
-        userMap.get(userID).changeEmail(newEmailAddress)
+    public void changeUserEmailAddress(int userID, String newEmailAddress) {
+        userMap.get(userID).setUserEmail(newEmailAddress);
     }
 
     //change user Phone Number - get info hash map
-    public changeUserPhoneNumber(String userID, String newPhoneNumber) {
-        userMap.get(userID).changePhoneNumber(newPhoneNumber)
+    public void changeUserPhoneNumber(int userID, String newPhoneNumber) {
+        userMap.get(userID).setPhoneNumber(newPhoneNumber);
     }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public LocalDate getJoiningDate() {
-        return joiningDate;
-    }
-
-    public void setJoiningDate(LocalDate joiningDate) {
-        this.joiningDate = joiningDate;
-    }
-
-
 }
+
+
+
