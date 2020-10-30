b93c67415f5e32722466e8459b375a97c7a4f7e7
Georgiana Marian <georgi.marian@gmail.com>
2020-10-30 14:07:37 +0200

    Added main file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// data structure is created here
+		User newUser = new User();
+		newUser.addNewUserProfile("bukky", "bukky@", "39747");
+		newUser.displayUserProfile(1);
+
+	}
+
+}
